5f7849f8697293abcad487b6d367e93a1ecf3a4e
wans <guyun.hy@gmail.com>
2017-08-10 00:22:06 +0800

    Recover panic stack

--- httpserver/wrapper.go
+++ httpserver/wrapper.go
@@ -5,32 +5,34 @@ import (
 	"fxlibraries/loggers"
 	"net/http"
 	"net/url"
+	"runtime"
 	"time"
 )
 
 // handlerWrapper Every http api hundler will be processed by this wrapper
 func HandlerWrapper(f HandleFunc) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var response *Response
+		request := NewRequest(r)
+		startTime := time.Now()
 		defer func() {
 			if r := recover(); r != nil {
-				loggers.Error.Println(r)
-				NewResponseWithError(errors.InternalServerError)
+				recoverError()
+				response = NewResponseWithError(errors.InternalServerError)
+				response.Write(w)
 			}
+			loggers.Info.Printf(`%3d %s %s %s %v`,
+				response.HTTPCode,
+				response.Code,
+				request.Method,
+				getPathAndQuery(r.URL),
+				time.Since(startTime),
+			)
 
 		}()
-		startTime := time.Now()
-
-		request := NewRequest(r)
-		response := f(request)
+		response = f(request)
 		response.Write(w)
 
-		loggers.Info.Printf(`%3d %s %s %s %v`,
-			response.HTTPCode,
-			response.Code,
-			request.Method,
-			getPathAndQuery(r.URL),
-			time.Since(startTime),
-		)
 	}
 }
 
@@ -42,4 +44,11 @@ func getPathAndQuery(u *url.URL) string {
 		rawQuery, _ := url.QueryUnescape(u.RawQuery)
 		return u.Path + "?" + rawQuery
 	}
+}
+
+func recoverError() {
+	buf := make([]byte, 1024)
+	n := runtime.Stack(buf, false)
+	loggers.Error.Printf("%s", buf[:n])
+	loggers.Error.Printf("End here")
 }
